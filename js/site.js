c785eb7266f3f5f32671976fd74b554bad099be7
scuevas1 <scuevas1@hawk.iit.edu>
2024-04-20 23:42:22 -0500

    adding the revised nav bar to other pages

--- js/site.js
+++ js/site.js
@@ -22,3 +22,13 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 menu.addEventListener('click',() => {
   navbarLinks.classList.toggle('active')
 })
+
+var header = document.getElementById("navbar-links");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
