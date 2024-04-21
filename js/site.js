25f1114306823882c8888626643bb68204c32a54
scuevas1 <scuevas1@hawk.iit.edu>
2024-04-21 13:37:08 -0500

    testing scroll back up button

--- js/site.js
+++ js/site.js
@@ -22,24 +22,3 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 menu.addEventListener('click',() => {
   navbarLinks.classList.toggle('active')
 })
-
-
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
